c416d44ea4e7ee0cdcf45192ee42f1478401286a
Teto <teto9001@gmail.com>
2012-10-26 11:03:52 +0300

    Update www/index.php

--- www/index.php
+++ www/index.php
@@ -3,27 +3,21 @@
 
     <div class="container">
      <div class="span8 offset2 well">
-      <h1 style="color:#D00;">WebGate</h1>
+      <h1 style="color:#D00;">piOpen</h1>
       Uptime: <?php   $data = shell_exec('uptime');
   $uptime = explode(' up ', $data);
   $uptime = explode(',', $uptime[1]);
   $uptime = $uptime[0].', '.$uptime[1]; 
   print $uptime; ?><br/>
-      Versione top.php<br/>
-      Versione bottom.php<br/>
-      Versione GPIO:<br/>
-      Versione me:<br/>
-      Temperatura:<br/>
-      Carico CPU:     <br/>
 
-      <a class="btn btn-primary" id="accendi">Accendi</a>
+      <a class="btn btn-primary" id="accendi">Open</a>
       <script>
         $("#accendi").click(function(){
 		$(this).text("...");
             $.ajax({
               url: "includes/accendi.php?tempo=1000"
             }).done(function(data) { 
-               $("#accendi").text("OK");
+               $("#accendi").text("Gate now open");
             });
         });
       </script>
