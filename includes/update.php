ce26202a9de4facd30dfcba13cb965231f7d548b
Teto <teto9001@gmail.com>
2012-10-27 12:25:24 +0200

    added 2nd repo to update.php

--- includes/update.php
+++ includes/update.php
@@ -1,7 +1,14 @@
 <?php
 require_once "git.php";
 $repo = Git::open('/var/www');
+print "<b>www</b><br>";
 print $repo->run('reset --hard');
 print '<br>';
 print $repo->run('pull origin');
+print '<br>';
+$repo2 = Git::open('/var/piopen');
+print "<b>shellScripts</b><br>";
+print $repo2->run('reset --hard');
+print '<br>';
+print $repo2->run('pull origin');
 ?>
