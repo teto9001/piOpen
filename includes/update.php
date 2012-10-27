17e38ac4c0ac4965e80ed817330a1ef6eae72100
Teto <teto9001@gmail.com>
2012-10-27 11:09:34 +0200

    x

--- includes/update.php
+++ includes/update.php
@@ -1,11 +1,7 @@
 <?php
-//set_time_limit(600);
-//print_r(str_replace("\n","<br>",shell_exec('cd /var/www && git reset --hard && git pull origin')));
 require_once "git.php";
 $repo = Git::open('/var/www');
 print $repo->run('reset --hard');
 print '<br>';
 print $repo->run('pull origin');
-
-
 ?>
